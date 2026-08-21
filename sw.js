4ab2b4c00701924cd2ee9d93818745966851836a
nikitsmm <nik.it.smm@gmail.com>
2026-08-21 14:40:02 +0300

    v44 - сначала центрифуга, потом яндекс диск

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    телефон в сети, всегда грузится свежее; кэш нужен только когда сети
    вообще нет. */
 
-var CACHE = 'pcsport-v43';
+var CACHE = 'pcsport-v44';
 var SHELL = [
   './',
   'index.html',
