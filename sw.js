9a32c81cbcc3836960527776fca6db72903ae159
nikitsmm <nik.it.smm@gmail.com>
2026-08-22 01:22:19 +0300

    v50 - new video message

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    телефон в сети, всегда грузится свежее; кэш нужен только когда сети
    вообще нет. */
 
-var CACHE = 'pcsport-v49';
+var CACHE = 'pcsport-v50';
 var SHELL = [
   './',
   'index.html',
