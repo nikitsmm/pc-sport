fb1d0431850b162845ecab48031e8dce45218427
nikitsmm <nik.it.smm@gmail.com>
2026-08-23 11:40:09 +0300

    v54 - поломка поля ввода текста в чате

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    телефон в сети, всегда грузится свежее; кэш нужен только когда сети
    вообще нет. */
 
-var CACHE = 'pcsport-v53';
+var CACHE = 'pcsport-v54';
 var SHELL = [
   './',
   'index.html',
